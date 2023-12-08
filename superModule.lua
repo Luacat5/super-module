ce111fcf923a95fa59d98e171dc28f407ba2dfd8
Ethan Durham <ethandurham@vl965-172-31-138-164.wireless.umass.edu>
2023-12-07 19:59:28 -0500

    added class.lua

--- superModule.lua
+++ superModule.lua
@@ -21,13 +21,10 @@ local module = {}
 function module.init(class,superClass)
     class._super = superClass
     -- to identify super class later
-    -- so don't name anything _super inside your class please
-    -- I would freeze this if I could..
+
     return function(inheritance)
         inheritance.new =  inheritance.new or false
-        -- aint gonna happen, unless you specify.. 
-        -- you creep
-
+        
         for i,v in pairs(superClass) do
             if type(v) == "function" and inheritance[i] ~= false then
                 class[i] = v
